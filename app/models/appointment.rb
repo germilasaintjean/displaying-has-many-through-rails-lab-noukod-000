6f23fe45491908df1bd68513b75d81d3d242fcea
Saint-Jean Germila <germila@noukod.com>
2019-02-19 05:27:10 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -2,10 +2,8 @@ class Appointment < ActiveRecord::Base
 belongs_to :doctor
 belongs_to :patient
 
-def set_doctor
-  @appointment = Appointment.find params[:doctor]
-end
-def set_patient
-  @appointment = Appointment.find params[:patient]
+def appointment
+  @appointment = Appointment.find params[:doctor_id]
+  @appointment = Appointment.find params[:patient_id]
 end
 end
