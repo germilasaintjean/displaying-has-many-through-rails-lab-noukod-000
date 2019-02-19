ba59ae9e5f48f2938b812fb4d47b23db2fe08006
Saint-Jean Germila <germila@noukod.com>
2019-02-19 06:03:56 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,11 +3,11 @@ belongs_to :doctor
 belongs_to :patient
 
 def appointment
-  # self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
-  # self.doctor.name
-  # self.patient.name
-  appointment = Appointment.find params[:doctor_id]
-  appointment = Appointment.find params[:patient_id]
-  self.appointment
+  # # self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
+  # # self.doctor.name
+  # # self.patient.name
+  # appointment = Appointment.find params[:doctor_id]
+  # appointment = Appointment.find params[:patient_id]
+  # self.appointment
 end
 end
