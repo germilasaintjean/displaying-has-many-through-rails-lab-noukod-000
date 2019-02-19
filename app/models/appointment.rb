371d5ba885cc4ca3ce30c365729c73621cf0f95c
Saint-Jean Germila <germila@noukod.com>
2019-02-19 06:02:44 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -6,7 +6,7 @@ def appointment
   # self.appointment_datetime.strftime("%B %d, %Y at %H:%M")
   # self.doctor.name
   # self.patient.name
-   @appointment = Appointment.find params[:doctor_id]
-  @appointment = Appointment.find params[:patient_id]
+   appointment = Appointment.find params[:doctor_id]
+  appointment = Appointment.find params[:patient_id]
 end
 end
