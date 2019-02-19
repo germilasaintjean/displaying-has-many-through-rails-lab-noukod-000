ff59f0d816088d4dd37828058613fc48f27ba0fa
Saint-Jean Germila <germila@noukod.com>
2019-02-19 05:28:34 +0000

    Automatically backed up by Learn

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,7 +3,7 @@ belongs_to :doctor
 belongs_to :patient
 
 def appointment
-  @appointment = Appointment.find params[:doctor_id]
-  @appointment = Appointment.find params[:patient_id]
+  # @appointment = Appointment.find params[:doctor_id]
+  #@appointment = Appointment.find params[:patient_id]
 end
 end
